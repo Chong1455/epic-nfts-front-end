13a7c51ec6d4c6a650d8a5c634abbc42bd4d3b01
Chong Kok Sian <ckh_5316@yahoo.com>
2022-02-07 11:08:44 +0800

    added view collections

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import './styles/App.css';
+import React, { useEffect, useState } from "react";
+import "./styles/App.css";
 import { ethers } from "ethers";
-import twitterLogo from './assets/twitter-logo.svg';
+import twitterLogo from "./assets/twitter-logo.svg";
 import myEpicNFT from "./utils/MyEpicNFT.json";
+import { Rings } from "react-loader-spinner";
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = "_buildspace";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
-const OPENSEA_LINK = '';
+const OPENSEA_LINK =
+  "https://testnets.opensea.io/collection/squarenft-adfx4n8efn";
 const TOTAL_MINT_COUNT = 50;
 
 const CONTRACT_ADDRESS = "0x1654dEC5cC1F0E71ab20F73A5Fa93912706aa67e";
 
 const App = () => {
   const [currentAccount, setCurrentAccount] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   const checkIfWalletIsConnected = async () => {
     const { ethereum } = window;
@@ -25,7 +28,7 @@ const App = () => {
       console.log("We have the ethereum object", ethereum);
     }
 
-    const accounts = await ethereum.request({ method: 'eth_accounts' });
+    const accounts = await ethereum.request({ method: "eth_accounts" });
 
     if (accounts.length !== 0) {
       const account = accounts[0];
@@ -35,25 +38,27 @@ const App = () => {
     } else {
       console.log("No authorized account found");
     }
-  }
+  };
 
   const connectWallet = async () => {
     try {
       const { ethereum } = window;
 
       if (!ethereum) {
-        alert('Get Metamask');
+        alert("Get Metamask");
         return;
       }
 
-      const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
       console.log("Connected", accounts[0]);
       setCurrentAccount(accounts[0]);
       setupEventListener();
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   const setupEventListener = async () => {
     try {
@@ -62,11 +67,17 @@ const App = () => {
       if (ethereum) {
         const provider = new ethers.providers.Web3Provider(ethereum);
         const signer = provider.getSigner();
-        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNFT.abi, signer);
+        const connectedContract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          myEpicNFT.abi,
+          signer
+        );
 
         connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
-          console.log(from, tokenId.toNumber())
-          alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`)
+          console.log(from, tokenId.toNumber());
+          alert(
+            `Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`
+          );
         });
 
         console.log("Setup event listener!");
@@ -74,9 +85,9 @@ const App = () => {
         console.log("Ethereum object doesn't exist!");
       }
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   const askContractToMintNFT = async () => {
     try {
@@ -85,26 +96,37 @@ const App = () => {
       if (ethereum) {
         const provider = new ethers.providers.Web3Provider(ethereum);
         const signer = provider.getSigner();
-        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNFT.abi, signer);
+        const connectedContract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          myEpicNFT.abi,
+          signer
+        );
 
         console.log("Going to pop wallet now to pay gas...");
         let nftTxn = await connectedContract.makeAnEpicNFT();
 
         console.log("Mining...Please wait.");
+        setIsLoading(true);
         await nftTxn.wait();
 
-        console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`)
+        console.log(
+          `Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`
+        );
+        setIsLoading(false);
       } else {
         console.log("Ethereum object doesn't exist!");
       }
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   // Render Methods
   const renderNotConnectedContainer = () => (
-    <button onClick={connectWallet} className="cta-button connect-wallet-button">
+    <button
+      onClick={connectWallet}
+      className="cta-button connect-wallet-button"
+    >
       Connect to Wallet
     </button>
   );
@@ -121,12 +143,30 @@ const App = () => {
           <p className="sub-text">
             Each unique. Each beautiful. Discover your NFT today.
           </p>
+          <p className="sub-text">
+            🌊View{" "}
+            <a target="_blank" href={OPENSEA_LINK}>
+              Collection
+            </a>{" "}
+            on OpenSea
+          </p>
           {currentAccount === "" ? (
             renderNotConnectedContainer()
           ) : (
-            <button onClick={askContractToMintNFT} className="cta-button connect-wallet-button">
+            <button
+              onClick={askContractToMintNFT}
+              className="cta-button connect-wallet-button"
+            >
               Mint NFT
             </button>
+          )}
+          {isLoading && (
+            <div className="loaderContainer">
+              <Rings color="#00BFFF" height={80} width={80} />
+              <p className="sub-text gradient-text inline">
+                Mining in progress...
+              </p>
+            </div>
           )}
         </div>
         <div className="footer-container">
